0422b9dc6c6978576ceba13ebe51c8c41badf6da
JackRie <riemersmajackie@gmail.com>
2021-06-30 15:22:09 -0400

    create pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,40 @@
 import * as React from "react"
-import { Link as GatsbyLink } from "gatsby"
+import { useStaticQuery, graphql, Link as GatsbyLink } from "gatsby"
 import { Container, Stack, Box, Heading, Text, Link } from "@chakra-ui/react"
 
 const HomePage = () => {
+	const data = useStaticQuery(graphql`
+		{
+			allWpPost {
+				nodes {
+					id
+					title
+					excerpt
+					uri
+				}
+			}
+		}
+	`)
+
+	const { allWpPost } = data
+
 	return (
 		<Container maxW="xl" centerContent>
 			<Heading as={`h1`} m={5}>
 				#100DaysOfGatsby
 			</Heading>
 			<Stack spacing={8}>
-				<Box p={5} shadow="md" borderWidth="1px">
-					<Heading as={`h2`}>Hello World</Heading>
-					<Text>
-						<span dangerouslySetInnerHTML={{ __html: "<p>This is an excerpt</p>" }} />
-					</Text>
-					<Link as={GatsbyLink} to="#">
-						Read More More &rarr;
-					</Link>
-				</Box>
+				{allWpPost.nodes.map(post => (
+					<Box key={post.id} p={5} shadow="md" borderWidth="1px">
+						<Heading as={`h2`}>{post.title}</Heading>
+						<Text>
+							<span dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+						</Text>
+						<Link as={GatsbyLink} to={post.uri}>
+							Read More More &rarr;
+						</Link>
+					</Box>
+				))}
 			</Stack>
 		</Container>
 	)
